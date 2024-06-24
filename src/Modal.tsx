e1270e3a0744743a8aad9d5c9dde24944dd58e27
guilhermenasszup <guilherme.nass@zup.com.br>
2024-06-24 12:51:07 -0300

    lint

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 const Overlay = styled.div`
